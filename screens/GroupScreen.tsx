eef9893272ce2c7a9a9206b60bd89a46ac852a29
Eugenio Galioto <eu.galioto@gmail.com>
2021-05-04 19:10:00 +0200

    Group detail page header set

--- screens/GroupScreen.tsx
+++ screens/GroupScreen.tsx
@@ -1,12 +1,22 @@
 import { StackScreenProps } from '@react-navigation/stack';
 import * as React from 'react';
-import { StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native';
 
 import { RootStackParamList, Group } from '../types';
 import { mainBackgroundColor } from '../constants/Colors';
 import { useRoute } from '@react-navigation/core';
 import { useSelector } from 'react-redux';
 import { RootState } from '../redux/store';
+import { screenWidth } from '../constants/Layout';
+
+const renderMemberPreview = (memberProfilePicture: string) => {
+  return (
+    <Image 
+      source={{uri: memberProfilePicture}}
+      style={styles.memberPreview}
+    />
+  )
+}
 
 export default function GroupScreen({
   navigation
@@ -18,26 +28,52 @@ export default function GroupScreen({
   const group = groups.find((item) => item.id === 'aaaa') // TODO - this is dev only
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Image 
       source={{uri: group?.headerPicture}}
       style={styles.imageHeader}
       />
 
-      <View style={styles.headerContainer}></View>
+      <View style={styles.headerContainer}>
+        <Text>{group?.title}</Text>
+        <Text>{`${group?.groupAccess} GROUP - ${group?.membersAmount} MEMBERS`}</Text>
+        <View style={styles.membersPreviewContainer}>
+          {group?.firstTenMembers.map((groupMember) => renderMemberPreview(groupMember.profilePicture))}
+        </View>
+      </View>
 
       <View style={styles.listAnItemContainer}></View>
 
       <Text style={styles.inYourGroupLabel}>IN YOUR GROUP</Text>
 
       <View style={styles.itemsContainer}></View>
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  membersPreviewContainer: {
+    flexDirection: 'row'
+  },
+  headerContainer: {
+    width: '95%',
+    backgroundColor: 'white',
+    padding: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 120,
+    top: -60
+  },
+  memberPreview: {
+    width: 28,
+    height: 28,
+    borderRadius: 14,
+    borderWidth: 1,
+    borderColor: 'white'
+  },
   imageHeader: {
-    width: '100%'
+    width: screenWidth,
+    height: screenWidth
   },
   container: {
     flex: 1,
